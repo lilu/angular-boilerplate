3e69780e64b53077bc43c3da6fc60bd40417ce87
niloop <lilu.life@me.com>
2018-06-07 18:38:36 +0800

    switch to scss

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
@@ -18,7 +18,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes() {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 
   add(name: string) {
@@ -28,11 +28,11 @@ export class HeroesComponent implements OnInit {
     }
     this.heroService
       .addHero({ name } as Hero)
-      .subscribe((hero) => this.heroes.push(hero));
+      .subscribe(hero => this.heroes.push(hero));
   }
 
   delete(hero: Hero) {
-    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
 }
