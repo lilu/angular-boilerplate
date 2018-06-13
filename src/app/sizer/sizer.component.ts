ce35dbba1a0aeecc0a50e3c45c325ed1c615ca51
niloop <lilu.life@me.com>
2018-06-13 19:58:58 +0800

    NgClass and NgStyle

--- src/app/sizer/sizer.component.ts
+++ src/app/sizer/sizer.component.ts
@@ -9,9 +9,24 @@ export class SizerComponent {
   @Input() size: number | string = 0;
   @Output() sizeChange = new EventEmitter<number>();
 
+  currentClasses = {
+    saveable: true,
+    modified: true,
+    special: false
+  };
+
+  currentStyles = {};
+
+  setStyles() {
+    this.currentStyles = {
+      'font-size': `${this.size}px`
+    };
+  }
+
   dec() {
     this.resize(-1);
   }
+
   inc() {
     this.resize(+1);
   }
@@ -19,5 +34,6 @@ export class SizerComponent {
   resize(delta: number) {
     this.size = Math.min(40, Math.max(8, +this.size + delta));
     this.sizeChange.emit(this.size);
+    this.setStyles();
   }
 }
